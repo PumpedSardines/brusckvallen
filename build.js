803e01a37e2ea8b39cff2ad73ccfa85b49cfb4ed
Fritiof Rusck <fritiof@rusck.se>
2024-08-18 17:05:56 +0200

    feat: changes

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const seedrandom = require("seedrandom");
 const path = require("path");
 const ejs = require("ejs");
 const sass = require("sass");
@@ -38,11 +39,21 @@ const paths = {
       const js = loaders.file(path.join(root, "js/default.js"));
 
       return minify.js(await js());
-    }
+    },
+    "home.js": async () => {
+      const js = loaders.file(path.join(root, "js/pages/home.js"));
+
+      return minify.js(await js());
+    },
   },
   styles: {
     "default.css": async () => {
       const css = loaders.scss(path.join(root, "styles/default.scss"));
+
+      return minify.css(await css());
+    },
+    "fonts.css": async () => {
+      const css = loaders.scss(path.join(root, "styles/fonts.scss"));
 
       return minify.css(await css());
     },
@@ -54,7 +65,7 @@ const paths = {
   },
   "home.html": async () => {
     const output = await ejs.renderFile(
-      path.join(root, "html/pages/home/index.html"),
+      path.join(root, "html/pages/home/index.ejs"),
       {},
       { async: true },
     );
@@ -88,7 +99,71 @@ async function build(folder, paths) {
   }
 }
 
-build(out, paths).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+function randomString(seed, length = 6) {
+  const rng = seedrandom(seed);
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars[Math.floor(rng() * chars.length)];
+  }
+  return result;
+}
+
+(async () => {
+  await build(out, paths).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  return;
+
+  const stylesFiles = fs.readdirSync(path.join(out, "styles"));
+  const htmlFiles = fs
+    .readdirSync(path.join(out))
+    .filter((file) => file.endsWith(".html"));
+
+  const variables = new Map();
+  const taken = new Set();
+  for (const file of stylesFiles) {
+    const content = fs.readFileSync(path.join(out, "styles", file), "utf-8");
+    content.match(/\.([a-zA-Z__-][a-zA-Z0-9__-]*)/g).forEach((match) => {
+      const rawName = match.slice(1);
+
+      if (variables.has(rawName)) {
+        return;
+      }
+
+      const classNameLength = 4;
+
+      let name = randomString(rawName, classNameLength);
+      while (taken.has(name)) {
+        name = randomString(name, classNameLength);
+      }
+      taken.add(name);
+      variables.set(rawName, name);
+    });
+  }
+
+  const variablesSorted = Array.from(variables.entries()).sort(
+    ([a], [b]) => b.length - a.length,
+  );
+
+  for (const style of stylesFiles) {
+    let content = fs.readFileSync(path.join(out, "styles", style), "utf-8");
+
+    for (const [rawName, name] of variablesSorted) {
+      content = content.replace(new RegExp(rawName, "g"), `${name}`);
+    }
+
+    fs.writeFileSync(path.join(out, "styles", style), content);
+  }
+
+  for (const htmlFile of htmlFiles) {
+    let content = fs.readFileSync(path.join(out, htmlFile), "utf-8");
+
+    for (const [rawName, name] of variablesSorted) {
+      content = content.replace(new RegExp(rawName, "g"), `${name}`);
+    }
+
+    fs.writeFileSync(path.join(out, htmlFile), content);
+  }
+})();
