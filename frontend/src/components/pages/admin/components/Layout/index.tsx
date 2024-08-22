36864620f7761511d45f20a364eccff8df892ecb
Fritiof Rusck <fritiof@rusck.se>
2024-08-22 12:06:51 +0200

    feat: responsivness on admin page

--- frontend/src/components/pages/admin/components/Layout/index.tsx
+++ frontend/src/components/pages/admin/components/Layout/index.tsx
@@ -12,6 +12,7 @@ function Layout(props: LayoutProps) {
   return (
     <div className={styles.root}>
       <div className={styles.inner}>
+        <h1>{props.title}</h1>
         {props.aside && <aside className={styles.aside}>{props.aside}</aside>}
         <div className={styles.main}>
           <h1>{props.title}</h1>
